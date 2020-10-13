1c802a07aa3d01d7b32aa43d76daba918f859c5b
Ktulu1 <ktulu_1@yahoo.com>
2020-10-13 06:43:33 -0400

    Regex Search

---
+++ regex_search.py
@@ -0,0 +1,20 @@
+# text file search
+# plain text and regex
+# command line arguments are filename and search string 
+
+import re
+from sys import argv
+
+script, filename, searchstr = argv
+text = []
+linenum = 0                                              
+pattern = re.compile(searchstr, re.IGNORECASE)              # create a pattern object called "pattern" from "searchstr" and ignore case
+with open (filename, 'rt') as searchfile:                   # open searchfile, keep it open, close it when done
+    for line in searchfile:                                 # loop "line" in "searchfile"
+        linenum += 1                                        # increment linenumber
+        if pattern.search(line) != None:                    # search line for "pattern" if not equal to none do next
+            text.append((linenum, line.rstrip('\n')))       # append list "text" with line numbers and line text with striped linefeeds
+for results in text:                                        # loop "results" in list "text"
+    print("Line", str(results[0]), ": " + results[1])       # print "results" with line numbers
+
+
